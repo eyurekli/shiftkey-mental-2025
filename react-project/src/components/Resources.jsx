eaac33b257e332ac249d3829cb5df632fb126a09
yurekli <kurumizawa38@gmail.com>
2025-01-18 17:57:49 -0400

    Updated resources contents

--- react-project/src/components/Resources.jsx
+++ react-project/src/components/Resources.jsx
@@ -6,10 +6,9 @@ export default function Resources()
         <>
          <nav id="resources">
             <h3>Resources</h3>
-            <p>Res 1</p>
-            <p>Res 2</p>
-            <p>Res 3</p>
-            <p>Res 4</p>
+            <p>Resource 1</p>
+            <p>Guide 2</p>
+            <p>Lesson 3</p>
          </nav>
         </>
     )
