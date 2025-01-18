303ae6ee3ecebcad259db4149b49ee5a677e8eaa
yurekli <kurumizawa38@gmail.com>
2025-01-18 17:35:39 -0400

    Button returned

--- react-project/src/components/Header.jsx
+++ react-project/src/components/Header.jsx
@@ -8,6 +8,7 @@ export default function Header()
                 <h1>Calmora</h1>
                 <div id="links">
                     <h3><a href="https://www.github.com" target="_blank">About Us</a></h3>
+                    <button id="mode-switch">Dark Mode</button>
                 </div>
             </header>
         </>
