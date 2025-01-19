c20488688a3a105ff2da8783aee79a43a8e0bdb6
niaxi <niaxidos@gmail.com>
2025-01-19 11:23:04 -0400

    added disclaimer

--- react-project/src/components/Header.jsx
+++ react-project/src/components/Header.jsx
@@ -5,7 +5,8 @@ export default function Header()
     return (
         <>
             <header>
-                <h1>Calmora</h1>
+                <h1>ConQuest</h1>
+                <h2 id = "phrase">Conquer your Social Anxiety</h2>
                 <div id="links">
                     <h3><a href="https://github.com/eyurekli/shiftkey-mental-2025" target="_blank">About Us</a></h3>
                 </div>
