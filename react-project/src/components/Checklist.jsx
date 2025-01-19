c225058ddcbeb37fabb8a4aa5d0b0988f2debe1c
yurekli <kurumizawa38@gmail.com>
2025-01-18 22:37:10 -0400

    Styling changes

--- react-project/src/components/Checklist.jsx
+++ react-project/src/components/Checklist.jsx
@@ -3,13 +3,11 @@ import "../styles/Checklist.css"
 export default function Checklist()
 {
     return (
-        <>
-         <nav id="checklist">
+        <nav id="checklist">
             <h3>Checklist</h3>
             <ul>
                 <li>Task: Go outside with friends</li>
             </ul>
-         </nav>
-        </>
+        </nav>
     )
 }
